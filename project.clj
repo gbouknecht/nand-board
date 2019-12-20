ccc72669535aeae155ec0edd64c3bb4f7209453f
Gerrit N. Bouknecht <gbouknecht@server.fake>
2019-12-20 11:44:35 +0100

    Add Midje test framework dependency
    Instead of putting the (unit) test files in a separate test directory I choose
    to put the test file next to the file (namespace) it is testing. By doing this
    we do not have to duplicate the src directory structure into the test directory
    anymore. Furthermore, I think it is handy to have the tests near by. And if you
    see these tests as kind of a specification (by example) of your code, why not
    put them together.
    
    The :jar-exclusions and :uberjar-exclusions were added to the project.clj to
    exclude the test files from the (uber)jars. I seems that these exclusions
    weren't effective for the :uberjar :aot resulting in a not found of the midje
    (dev) dependency. To solve this, the namespace to compile ahead of time
    (nand-board.core) is now explicitly specified. The assumption is that not only
    this namespace is compiled but also all the namespaces it is depending on.

--- project.clj
+++ project.clj
@@ -1,9 +1,14 @@
+(def ^:private test-file-pattern #".*_test.clj")
+
 (defproject nand-board "0.1.0-SNAPSHOT"
   :description "Nand Board - Playing with NAND gates on a board"
   :url "http://server.fake/nand-board"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+            :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
   :main ^:skip-aot nand-board.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot [nand-board.core]}
+             :dev     {:dependencies [[midje "1.9.9"]]}}
+  :jar-exclusions ~[test-file-pattern]
+  :uberjar-exclusions ~[test-file-pattern])
---
+++ src/nand_board/core_test.clj
@@ -0,0 +1,6 @@
+(ns nand-board.core-test
+  (:require [midje.sweet :refer [fact =>]]))
+
+(fact
+  "about a failing test that needs to be fixed"
+  0 => 1)
