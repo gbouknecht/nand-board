2516b33e567bfd59ad1296fa1f1b0454213c7d55
Gerrit N. Bouknecht <gbouknecht@server.fake>
2020-01-19 13:49:48 +0100

    Add specs for state data structure

---
+++ src/nand_board/logic/state_spec.clj
@@ -0,0 +1,26 @@
+(ns nand-board.logic.state-spec
+  (:require [clojure.set :as set]
+            [clojure.spec.alpha :as s]
+            [nand-board.logic.board-spec :as board-spec]))
+
+(defn- difference [coll1 coll2]
+  (set/difference (set coll1) (set coll2)))
+
+(defn- board-pin-ids [state]
+  (keys (:pins (:board state))))
+
+(defn- vals-pin-ids [state]
+  (keys (:vals state)))
+
+(defn- event-queue-pin-ids [state]
+  (map :pin-id (:event-queue state)))
+
+(s/def ::time (s/and int? (s/nonconforming (s/or :zero zero? :positive pos?))))
+(s/def ::val #{0 1})
+(s/def ::vals (s/map-of ::board-spec/pin-id ::val))
+(s/def ::event (s/keys :req-un [::time ::board-spec/pin-id ::val]))
+(s/def ::event-queue (s/coll-of ::event :kind set?))
+(s/def ::state (s/and (s/keys :req-un [::time ::board-spec/board ::vals ::event-queue])
+                      #(empty? (difference (vals-pin-ids %) (board-pin-ids %)))
+                      #(empty? (difference (event-queue-pin-ids %) (board-pin-ids %)))
+                      #(->> % :event-queue (map :time) (every? (fn [t] (<= (:time %) t))))))
---
+++ src/nand_board/logic/state_spec_test.clj
@@ -0,0 +1,35 @@
+(ns nand-board.logic.state-spec-test
+  (:require [clojure.spec.alpha :as s]
+            [midje.sweet :refer [=> facts]]
+            [nand-board.logic.board :refer [add-gate make-initial-board]]
+            [nand-board.logic.state-spec :as state-spec]))
+
+(facts
+  "state data structure"
+
+  (s/valid? ::state-spec/time -1) => false
+  (s/valid? ::state-spec/time 0) => true
+  (s/valid? ::state-spec/time 0.5) => false
+  (s/valid? ::state-spec/time 1) => true
+  (s/valid? ::state-spec/time 2) => true
+  (s/valid? ::state-spec/time "2") => false
+
+  (s/valid? ::state-spec/val -1) => false
+  (s/valid? ::state-spec/val 0) => true
+  (s/valid? ::state-spec/val 1) => true
+  (s/valid? ::state-spec/val 2) => false
+
+  (let [board (-> (make-initial-board) add-gate add-gate)
+        state {:time 3
+               :board board
+               :vals {0 0, 2 1, 3 0, 4 0, 5 1}
+               :event-queue #{{:time 3 :pin-id 2 :val 1}
+                              {:time 4 :pin-id 0 :val 1}
+                              {:time 4 :pin-id 1 :val 1}
+                              {:time 5 :pin-id 4 :val 1}
+                              {:time 9 :pin-id 2 :val 0}}}]
+    (s/valid? ::state-spec/state {:time 0 :board (make-initial-board) :vals {} :event-queue #{}}) => true
+    (s/valid? ::state-spec/state state) => true
+    (s/valid? ::state-spec/state (assoc-in state [:vals] {6 0})) => false
+    (s/valid? ::state-spec/state (update-in state [:event-queue] conj {:time 5 :pin-id 6 :val 1})) => false
+    (s/valid? ::state-spec/state (update-in state [:event-queue] conj {:time 2 :pin-id 0 :val 1})) => false))
